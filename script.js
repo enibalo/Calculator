fbd06d0365631aa81a12de8bea9b6128c4d41029
enibalo <49178664+enibalo@users.noreply.github.com>
2023-06-24 12:50:52 -0600

    Create functions for calculator

---
+++ script.js
@@ -0,0 +1,34 @@
+function add(a,b){
+    return a + b;
+}
+
+function subtract(a,b){
+    return a - b;
+}
+
+function multipily(a,b){
+    return a* b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+let num;
+let operator;
+let num2;
+
+function operate(num,operator,num2){
+    if (operator == "add"){
+        add(num,num2);
+    }
+    else if(operator =="subtract"){
+        subtract(num,num2);
+    }
+    else if(operator == "multiply"){
+        multipily(num,num2);
+    }
+    else{
+        divide(num,num2);
+    }
+}
