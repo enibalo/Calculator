2d036902715a599109646ac6da04bce27a0e8013
enibalo <49178664+enibalo@users.noreply.github.com>
2023-07-05 16:47:37 -0600

    Connect script to html page so calcultor can perform operations with two operands and so all buttons fufill their functions

--- script.js
+++ script.js
@@ -19,17 +19,44 @@ let operator;
 let num2;
 
 function operate(num,operator,num2){
-    if (operator == "add"){
-        add(num,num2);
+    if (operator == "+"){
+        return "" + add(num,num2);
     }
-    else if(operator =="subtract"){
-        subtract(num,num2);
+    else if(operator =="−"){
+        return "" + subtract(num,num2);
     }
-    else if(operator == "multiply"){
-        multipily(num,num2);
+    else if(operator == "×"){
+        return "" + multipily(num,num2);
     }
     else{
-        divide(num,num2);
+        return "" + divide(num,num2);
     }
 }
+
+const display = document.querySelector(".display");
+
+const buttons = Array.from(document.querySelectorAll(".num"));
+buttons.forEach(function(btn){
+    btn.onclick = function (){
+        display.textContent = display.textContent + btn.textContent;
+}})
+
+const symbols = Array.from(document.querySelectorAll(".symbol"));
+symbols.forEach(function(symbol){
+    symbol.onclick = function(){
+        display.textContent = display.textContent + " " + symbol.textContent + " ";
+    }
+})
+
+const equal = document.querySelector(".equal");
+equal.onclick = function(){
+    let arr = display.textContent.split(" ");
+    display.textContent = operate(arr[0], arr[1], arr[2]); 
+}
+
+const del = document.querySelector(".delete");
+del.addEventListener("click", function clear(){
+    display.textContent = "";
+});
+
 
